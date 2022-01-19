9c01390c87d1512ae0e93e774735c2772b4f5ab6
bwillink <bwillink@gmail.com>
2022-01-19 22:09:49 +0800

    node-dating MAP tree

--- scripts/node_dating/run_node_MAP.sh
+++ scripts/node_dating/run_node_MAP.sh
@@ -1,5 +1,9 @@
 #!bin/bash
 
+# burned trees before loading on rb or it will crash
+cat ../output/node_dating/Coen.g0.fossil.calib.flat.973180.trees | tail -6001 > ../output/node_dating/Coen.g0.fossil.calib.flat.973180.burned.trees
+
+
 # run the dating analysis
 rb_command="source(\"./node_dating/node_MAP.Rev\");"
 echo $rb_command | ~/Applications/revbayes-development/projects/cmake/rb
